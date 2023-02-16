67f245f1a4b96140325b675cfb981cc594a0487e
Áuria Lima <122231764+AuriaLima@users.noreply.github.com>
2023-02-15 23:48:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function validar(){
+var número A = document.getElementById('número A')
+var número B = document.getElementById('número B')
+var numA = Number(Número A .value)
+var numB = Number(Número B .value)
+
+if (numB > numA) {
+    console.log("Formulário válido")
+}
+else {
+    console.log("Formulário inválido")
+}
+}
+
+
+
+
+
+
+
